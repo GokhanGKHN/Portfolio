77e7825aa7138bf6d9a1d6864582b717ff761ace
GokhanGKHN <109904335+GokhanGKHN@users.noreply.github.com>
2023-02-26 18:42:08 +0300

    Main

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,6 +96,22 @@ modalCloses.forEach((modalClose)=>{
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
 
 /*==================== TESTIMONIAL ====================*/
 
